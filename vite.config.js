d4e300e0b4c2070605f880bfb1ac38bb95de0020
Mikhail Nekrasov <mixail.nekrasoff@gmail.com>
2023-11-13 18:36:51 +0300

    fix paths

--- vite.config.js
+++ vite.config.js
@@ -72,12 +72,12 @@ export default defineConfig({
         theme_color: "#1867C0",
         icons: [
           {
-            src: "icons/192.png",
+            src: "./icons/192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "icons/512.png",
+            src: "./icons/512.png",
             sizes: "512x512",
             type: "image/png",
           },
