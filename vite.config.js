a1ffc5182922882b84044a4ab2cbe880dfceddac
Mikhail Nekrasov <mixail.nekrasoff@gmail.com>
2023-11-13 18:22:43 +0300

    experiment with sw cache

--- vite.config.js
+++ vite.config.js
@@ -35,7 +35,7 @@ export default defineConfig({
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: "CacheOnly",
+            handler: "CacheFirst",
             options: {
               cacheName: "google-fonts-cache",
               cacheableResponse: {
@@ -45,7 +45,7 @@ export default defineConfig({
           },
           {
             urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-            handler: "CacheOnly",
+            handler: "CacheFirst",
             options: {
               cacheName: "gstatic-fonts-cache",
               cacheableResponse: {
@@ -55,7 +55,7 @@ export default defineConfig({
           },
           {
             urlPattern: /^https:\/\/cjrtnc\.leaningtech\.com\/2\.3\/.*/i,
-            handler: "CacheOnly",
+            handler: "CacheFirst",
             options: {
               cacheName: "cheerpj-cache",
               cacheableResponse: {
@@ -72,12 +72,12 @@ export default defineConfig({
         theme_color: "#1867C0",
         icons: [
           {
-            src: "./icons/192.png",
+            src: "icons/192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "./icons/512.png",
+            src: "icons/512.png",
             sizes: "512x512",
             type: "image/png",
           },
